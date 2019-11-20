042b318a328d2bb6b0dc0b531666238deb70420c
OsaidAyadi <osaid.step@gmail.com>
2019-11-20 17:10:32 +0200

    Ex2

---
+++ Loop_Exercise.py
@@ -0,0 +1,107 @@
+#Q1
+"""
+num1 = int(input('Enter the first number:'))
+num2 = int(input('Enter the second number:'))
+print (  "The greatest number is :"  ,  num1 if num1>num2 else num2)
+"""
+
+"""
+#Q2
+number = int (input("Input a number :-"))
+for i in range (1,11):
+    print(number , "x" , i , "=" , number*i)
+    
+"""   
+ 
+"""
+#Q3
+x=""
+for i in range (1,6):
+    x+="*"
+    print(x)
+    if (i==5):
+        for j in range(i,0,-1):
+            print(x[1:j])
+ """
+ 
+ 
+ 
+"""
+ #Q4
+ x
+ y
+ z
+ b
+ """
+ 
+"""
+  #Q5
+  
+  12
+  15
+  18
+  21
+  24
+  
+   """
+   
+"""
+  #Q5
+  
+  1
+  2
+  3
+  
+   """
+   
+   
+"""   
+   #Q6
+sum=0
+number= int (input("Enter number :"))
+for i in range (number+1):
+    sum+=i
+print(sum)   
+
+"""
+
+
+   
+
+"""
+
+   #Q7
+number= int (input("Enter number :"))
+if( number%2 == 0):
+    print("The number is even")
+else:
+    print("The number is odd")
+
+"""
+
+"""
+
+for i in range (0,9):
+    for o in range(10-i):
+        print (" ",end="  ")
+    for x in range(1,i):
+        print (x,end="  ")
+        if x == (i-1):
+           for j in range(i,0,-1):
+                print (j , end="  ")
+    print()
+for i in range (9,0,-1):
+    for o in range(10-i):
+        print (" ",end="  ")
+    for x in range(1,i):
+        print (x , end="  ")
+        if x==i-1:
+            for j in range(i,0,-1):
+                print (j , end="  ")
+    print()
+
+
+"""
+    
+
+    
