1b875d5c6585324745a2be681dc0686e29312ed4
OsaidAyadi <osaid.step@gmail.com>
2019-11-21 13:58:12 +0200

    Day 3 Python

---
+++ Day3_Exercise.py
@@ -0,0 +1,52 @@
+#Q1
+li = [1,2,3,4,5]
+for x in li:
+    print(x)
+    
+#Q2
+s= sum(li)
+print(s)    
+
+#Q3
+large = max(li)
+print(large)
+
+#Q4
+a = [10,20,30,20,10,50,60,40,80,50,40]
+b = list(set(a))
+print (b)
+
+#Q5
+
+emp = []
+if (a==emp):
+    print("The list is empty")
+else:
+    print ("The list isn't empty")    
+    
+    
+#Q6
+
+t = (1,2,"3",[1,2,3],{"Name":"Osaid","Age":24})    
+for i in t:
+    print (i)
+    
+#Q7
+num_set = set([0, 1, 2, 3, 4, 5])
+for i in num_set:
+    print(i)    
+
+#Q8
+s1 = {1,2,3,4,5,6,7}
+s2 = {3,7,8,4,22,7}
+print (s1 & s2)
+
+
+#Q9
+
+"""{green,blue,yellow}"""
+
+#Q10
+
+"""6"""
+    
